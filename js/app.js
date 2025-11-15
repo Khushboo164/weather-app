be6448dc5a0525ba154a3e1533de66069e1ac74f
khushboo pirwani <khushboopirwani@gmail.com>
2025-11-15 17:13:02 +0530

    Created basic HTML structure for the weather application

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// Getting elements
+const searchBtn = document.getElementById("search-btn");
+const cityInput = document.getElementById("city-input");
+
+const cityName = document.getElementById("city-name");
+const temperature = document.getElementById("temperature");
+const description = document.getElementById("weather-description");
+
+// Button click event
+searchBtn.addEventListener("click", () => {
+    const userCity = cityInput.value;
+
+    if (userCity === "") {
+        alert("Please enter a city name");
+        return;
+    }
+
+    // Showing sample data
+    cityName.textContent = "City: " + userCity;
+    temperature.textContent = "Temperature: 25°C";
+    description.textContent = "Weather: Clear Sky";
+});
